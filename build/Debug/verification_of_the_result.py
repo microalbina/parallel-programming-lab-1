490599676b019b45ab3695ad44d4828ceefe13ab
microalbina <shalimova.albina07@gmail.com>
2026-04-24 10:05:08 +0400

    Commit with deleted and overwritten txt files

--- build/Debug/verification_of_the_result.py
+++ build/Debug/verification_of_the_result.py
@@ -3,14 +3,14 @@ import numpy as np
 
 def verifyMatrices():
 
-    with open("original_matrices.txt", "r", encoding = "utf-8") as file1:
+    with open("D:/Projects/parallel-programming-lab-1/build/Debug/original_matrices.txt", "r", encoding = "utf-8") as file1:
         matrices = file1.read().split("\n\n")
 
         mat1 = np.array([[int(x) for x in row.split()] for row in matrices[0].strip().split("\n")])
         mat2 = np.array([[int(x) for x in row.split()] for row in matrices[1].strip().split("\n")])
 
 
-    with open("result_matrix.txt", "r", encoding = "utf-8") as file2:
+    with open("D:/Projects/parallel-programming-lab-1/build/Debug/result_matrix.txt", "r", encoding = "utf-8") as file2:
         matrix_lines = file2.readlines()[2:]
         matrix = ''.join(matrix_lines).strip()
 
